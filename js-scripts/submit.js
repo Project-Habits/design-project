a9af751bf2357560c8ac35d41a0dce3885a6dd4e
Galaxeo <hwjustincheok@gmail.com>
2023-10-12 16:31:35 -0400

    feat: add fastAPI post request compatability.

--- js-scripts/submit.js
+++ js-scripts/submit.js
@@ -1,23 +1,24 @@
-var fs = require('fs');
 const submitBut = document.querySelector("button[class='submitBut']");
 const formEle = document.querySelector("div[val]");
 let userData = {
-    table: []
+  table: []
 }
-submitBut.onclick = (event) => {
+submitBut.onclick = async (event) => {
   // Blur background behind the form
   event.preventDefault;
   formEle.classList.toggle('hidden');
   document.querySelector(".main").classList.toggle('blur');
   // Read the form values into variables
-  const taskEntry = formEle.querySelector("input[id='Workout Type?']").value;
-  const projectEntry = formEle.querySelector("input[id='Meal Goal?']").value;
-  console.log(taskEntry);
-  console.log(projectEntry);
+  const workoutEntry = formEle.querySelector("input[id='Workout Type?']").value;
+  const mealEntry = formEle.querySelector("input[id='Meal Goal?']").value;
+  fetch('http://127.0.0.1:8000', {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "workout": workoutEntry, "meal": mealEntry }),
+  })
+    .then(response => response.json())
+    .then(data => { console.log(data); })
 }
-userData.table.push({workout: taskEntry, meal: projectEntry});
-var json = JSON.stringify(userData);
-fs.writeFile("output.json", json, 'utf8', function(err) {
-    if (err) throw err;
-    console.log('complete');
-});
---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Recs(BaseModel):
+    workout: str
+    meal: str
+
+@app.post("/")
+def return_status(recs: Recs):
+    return recs
+
